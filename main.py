214872f28eb6966d65a941c9f30d95e88b9b99b9
Seiichi Inoue <d35inou108@gmail.com>
2019-02-13 01:37:20 +0900

    make changes on creating chainer datasets

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from chainer import backend
 from chainer import backends
 from chainer.backends import cuda
 from chainer import Function, gradient_check, report, training, utils, Variable
+from chainer.datasets import tuple_dataset
 from chainer import datasets, iterators, serializers
 from chainer import Link, Chain, ChainList
+from chainer.functions.loss.mean_squared_error import mean_squared_error
 import chainer.functions as F
 import chainer.links as L
 import chainer.optimizers as O
@@ -17,38 +19,37 @@ from chainer.training import extensions
 from models import convNet
 from music_processor import *
 
-def get_data(feats, answers, major_note_index, samplerate, soundlen=15, split=1):
+def get_data(feats, answers, major_note_index, samplerate, soundlen=15):
     """
     Args:
         feats: song.feats; Audio module
         answers: song.answers; Audio module
-        major_note_index: 
+        major_note_index: answer labels; corresponding to feats
         samplerate: song.samplerate; Audio module
         soundlen: =15. 学習モデルに渡す画像データの横方向の長さ．ここでは(80 * 15)サイズのデータを使用している
         split: =1. 
-    Valiables:
+    Variables:
         minspace: minimum space between major note indexs
         maxspace: maximum space between major note indexs
-        idx: index of feats
+        idx: index of major_note_index or feats
         dist: distance of two notes
-        data: train data of one song
     """
 
     minspace = 0.1
     maxspace = 0.7
     idx = np.random.permutation(major_note_index.shape[0] - soundlen) + soundlen // 2
-    data = []
+    X = []
+    y = []
 
-    for i in range(int(idx.shape[0] * split)):
+    for i in range(int(idx.shape[0])):
         dist = major_note_index[idx[i] + 1] - major_note_index[idx[i]]
         
         if dist < maxspace * samplerate / 512 and dist > minspace * samplerate / 512:    
             for k in range(-1, dist+2):
-                X = feats[:,:,major_note_index[idx[i]] - soundlen // 2 + k:major_note_index[idx[i]] + soundlen // 2 + k + 1]
-                y = answers[major_note_index[idx[i]] + k]
-                data.append((np.array(X), y))
+                X.append(feats[:,:,major_note_index[idx[i]] - soundlen // 2 + k:major_note_index[idx[i]] + soundlen // 2 + k + 1])
+                y.append(answers[major_note_index[idx[i]] + k])
     
-    return data        
+    return X, y
     
 
 def data_builder(songs, soundlen):
@@ -57,16 +58,29 @@ def data_builder(songs, soundlen):
         songs: the list of song
         soundlen: width of one train data's image 
     Variables:
-        train: the list of tuple; (X, y) for training with Chainer
+        data: the list of tuple; (X, y) for training with Chainer
     """
 
-    train = []
+    Xs = []
+    ys = []
     for song in songs:
-        train.append(get_data(song.feats, song.answer, song.major_note_index, song.samplerate, soundlen, split=0.2))
+        X, y = get_data(song.feats, song.answer, song.major_note_index, song.samplerate, soundlen)
+        Xs += X
+        ys += y
 
-    return train
+    data = tuple_dataset.TupleDataset(np.array(Xs, dtype='float32'), np.array(ys, dtype='float32'))
+
+    return data
 
 
+"""
+Training Process Flow:
+    1. from songs, make teacher labels(answer).
+    2. milden answer.
+    3. build chainer model.
+    4. build chainer trainer.
+    5. train model.
+"""
 
 def train(songs, epochs, soundlen, don_ka=0):
     """
@@ -75,7 +89,6 @@ def train(songs, epochs, soundlen, don_ka=0):
         epochs: number of train 
         soundlen: width of one train data's image
         don-ka: don(1) or ka(2) or both(0)
-
     """
 
     for song in songs:
@@ -83,8 +96,9 @@ def train(songs, epochs, soundlen, don_ka=0):
         print(song.timestamp)
         timing = song.timestamp[:, 0]
         sound = song.timestamp[:, 1]
-        song.answer = np.zeros((song.feats.shape[2]))
+        song.answer = np.zeros((song.feats.shape[2]))  # frameで切り分けた学習データの数分の答えを用意する
 
+        # don/ka diteciton and make teacher labels
         if don_ka == 0:
             song.major_note_index = np.rint(timing[np.where(sound != 0)] * song.samplerate / 512).astype(np.int32)
         else:
@@ -99,8 +113,8 @@ def train(songs, epochs, soundlen, don_ka=0):
 
 
     # build model
-    model = L.Classifier(convNet())
-
+    model = L.Classifier(convNet(), lossfun=mean_squared_error)
+    model.compute_accuracy = False  # not need accuracy cz teacher data is not label; using sigmoid func
     train = data_builder(songs, soundlen)
     test = data_builder(songs, soundlen)
     train_iter = iterators.SerialIterator(train, 256)
@@ -131,6 +145,3 @@ if __name__ == '__main__':
     #     songs = pickle.load(f)
 
         train(songs=songs, epochs=30, soundlen=15)
-
-
-
