38f55ca3913dc91635cdd5afc3896eaea84df036
Seiichi Inoue <d35inou108@gmail.com>
2019-02-18 16:00:28 +0900

    makes small changes on weight initializer

--- models.py
+++ models.py
@@ -23,6 +23,8 @@ class convNet(Chain):
     def __init__(self):
         super(convNet, self).__init__()
         with self.init_scope():
+            # weight initiarilze
+            I.Normal(scale=1.0)
             # convolution
             self.conv1 = L.Convolution2D(3, 10, ksize=(3,7))
             self.conv2 = L.Convolution2D(10, 20, 3)
@@ -41,3 +43,24 @@ class convNet(Chain):
         y = F.sigmoid(self.fc3(x))
         
         return y
+
+
+
+class pracNet(Chain):
+    """for practice chain."""
+
+    def __init__(self):
+        super(pracNet, self).__init__()
+        with self.init_scope():
+            self.fc1 = L.Linear(3600, 256)
+            self.fc2 = L.Linear(256, 120)
+            self.fc3 = L.Linear(120, 1)
+
+
+    def __call__(self, x):
+        x = F.dropout(F.relu(self.fc1(x)))
+        x = F.dropout(F.relu(self.fc2(x)))
+        
+        return F.sigmoid(self.fc3(x))
+
+
